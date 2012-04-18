c27293466a04a72043c375ac34dac5da9e26fa9a
joewilliams <williams.joe@gmail.com>
2012-04-18 10:03:11 -0700

    support for ets and dets info

--- include/folsom_cowboy.hrl
+++ include/folsom_cowboy.hrl
@@ -8,7 +8,9 @@
                           {[<<"_port">>], folsom_cowboy_port_handler, []},
                           {[<<"_process">>], folsom_cowboy_process_handler, []},
                           {[<<"_statistics">>], folsom_cowboy_statistics_handler, []},
-                          {[<<"_system">>], folsom_cowboy_system_handler, []}
+                          {[<<"_system">>], folsom_cowboy_system_handler, []},
+                          {[<<"_ets">>], folsom_cowboy_ets_handler, []},
+                          {[<<"_dets">>], folsom_cowboy_dets_handler, []}
                          ]
                    }
                   ]).
